9ee5cfddd685bc4846904827f4ee8f3e64b296a9
Usuario <Email Usuario>
2022-10-06 16:30:39 -0500

    hola

---
+++ index.js
@@ -0,0 +1,12 @@
+//TIPO DE INDEPENDECIAS
+//COMMONJS 
+//ES6MODULE
+//Tipos de variable
+
+//const sumar = require("./sumar")
+
+//let: variables
+let a = 10
+//const: constantes
+const b=20
+
---
+++ sumar.js
@@ -0,0 +1,6 @@
+//funcion para sumar
+function sumar(a , b){
+    return a + b
+}
+//exportar: commonjs
+module.exports = sumar
